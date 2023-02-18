eefb4d82a6f10cc79a78ba44d1214a552b424374
karimalik <karim.kompissi@outlook.fr>
2023-02-18 11:58:35 +0100

    modified

--- claudi.sql
+++ claudi.sql
@@ -25,14 +25,6 @@ INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
 -- Dumping data for table claudia-real-estate.failed_jobs: ~0 rows (approximately)
 
 -- Dumping data for table claudia-real-estate.migrations: ~0 rows (approximately)
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-	(1, '2014_10_12_000000_create_users_table', 1),
-	(2, '2014_10_12_100000_create_password_resets_table', 1),
-	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-	(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-	(5, '2023_02_11_071642_create_contacts_table', 1),
-	(6, '2023_02_15_115503_create_categories_table', 1),
-	(7, '2023_02_15_115711_propertie', 1);
 
 -- Dumping data for table claudia-real-estate.password_resets: ~0 rows (approximately)
 
@@ -48,9 +40,7 @@ INSERT INTO `properties` (`id`, `name`, `beds`, `baths`, `city`, `address`, `are
 	(7, 'Triplex', '9', '6', 'Quebec', '280 route 105, Gracefield, Qc', NULL, '+10', '2020', 'Super belle maison rénovée au fil du temps. Elle comprend 4 chambres à coucher au total dont 2 au sous-sol. Cette propriété a été aménagé avec un logement parental avec entrée indépendante permettant de générer un revenu supplémentaire. Présentement, les locataires payent 900$ par mois chauffé, éclairé et l\'internet (sans bail). Ils sont prêts à rester ou à quitter. Le logement parental peut être tout simplement enlevé et faire une salle familiale également', '1676698080-Triplex.jpg', NULL, 'Oui', 'Oui', NULL, '189 900', 1, '2023-02-18 04:28:00', '2023-02-18 04:28:00'),
 	(8, 'MAISON DE PLAIN-PIED', '4', '5', 'Quebec', '280 route 105, Gracefield, Qc', NULL, '9', '2012', 'Super belle maison rénovée au fil du temps. Elle comprend 4 chambres à coucher au total dont 2 au sous-sol. Cette propriété a été aménagé avec un logement parental avec entrée indépendante permettant de générer un revenu supplémentaire. Présentement, les locataires payent 900$ par mois chauffé, éclairé et l\'internet (sans bail). Ils sont prêts à rester ou à quitter. Le logement parental peut être tout simplement enlevé et faire une salle familiale également', '1676698127-MAISON DE PLAIN-PIED.jpg', NULL, 'Oui', 'Non', NULL, '1000 000', 2, '2023-02-18 04:28:48', '2023-02-18 04:28:48');
 
--- Dumping data for table claudia-real-estate.users: ~1 rows (approximately)
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-	(1, 'Admin', 'admin@admin.com', '2023-02-18 04:10:28', '$2y$10$svTWN1fp4EQWiwSdiSfSaeZRVEqpb.nOif44QWGcW3Ill71VX/cxq', 'lFH3kf9kkh', '2023-02-18 04:10:28', '2023-02-18 04:10:28');
+
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
